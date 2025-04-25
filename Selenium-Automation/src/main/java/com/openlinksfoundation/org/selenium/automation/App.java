ee359b45d9c939024864e702efb7618e56d9d172
Ishapotekar17 <ishwari.olf@gmail.com>
2025-04-25 18:05:09 +0530

    Update App.java

--- Selenium-Automation/src/main/java/com/openlinksfoundation/org/selenium/automation/App.java
+++ Selenium-Automation/src/main/java/com/openlinksfoundation/org/selenium/automation/App.java
@@ -204,6 +204,7 @@ public class App extends Application {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     
     @FXML
 private void run_TLM() {
@@ -396,6 +397,8 @@ public void selectFile(String path) throws AWTException {
     robot.delay(4000);
     robot.keyRelease(KeyEvent.VK_ENTER);
 }
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
